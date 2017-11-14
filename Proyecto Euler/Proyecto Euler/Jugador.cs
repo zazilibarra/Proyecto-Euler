cf64a8de7cdf875a9b5d22d7ad0399e13aa9d819
zazilibarra <zaziil.97@gmail.com>
2017-11-13 19:29:49 -0600

    Clases

---
+++ Proyecto Euler/Proyecto Euler/Jugador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Euler
+{
+    class Jugador
+    {
+        private int iID;
+        private int iIDReto;
+        private string sNombre;
+        private string sContraseña;
+
+        public Jugador()
+        {
+
+        }
+
+        public void contestar()
+        {
+
+        }
+
+        public void registrarAvance()
+        {
+
+        }
+    }
+}
---
+++ Proyecto Euler/Proyecto Euler/Reto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Euler
+{
+    class Reto
+    {
+        private int iID;
+        private List<Respuesta> lRespuestas;
+
+        public Reto()
+        {
+
+        }
+        
+        public void iniciar()
+        {
+
+        }
+
+        public bool evaluar()
+        {
+            return true;
+        }
+
+        public void avanzar()
+        {
+
+        }
+    }
+
+}
