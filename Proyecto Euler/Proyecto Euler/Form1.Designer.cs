d0d55b5da41ad81cd350468a03bf300edf70a441
k2rosvall <k2.rosas.v@gmail.com>
2017-11-13 21:07:42 -0600

    Pantalla Menu
    Se agrego la pantalla de menu con sus respectivos botones.

--- Proyecto Euler/Proyecto Euler/Form1.Designer.cs
+++ Proyecto Euler/Proyecto Euler/Form1.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btIniciar = new System.Windows.Forms.Button();
+            this.btContinuar = new System.Windows.Forms.Button();
+            this.btAyuda = new System.Windows.Forms.Button();
+            this.btRecords = new System.Windows.Forms.Button();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btIniciar
+            // 
+            this.btIniciar.Location = new System.Drawing.Point(23, 68);
+            this.btIniciar.Name = "btIniciar";
+            this.btIniciar.Size = new System.Drawing.Size(174, 40);
+            this.btIniciar.TabIndex = 0;
+            this.btIniciar.Text = "Iniciar";
+            this.btIniciar.UseVisualStyleBackColor = true;
+            // 
+            // btContinuar
+            // 
+            this.btContinuar.Location = new System.Drawing.Point(23, 126);
+            this.btContinuar.Name = "btContinuar";
+            this.btContinuar.Size = new System.Drawing.Size(174, 40);
+            this.btContinuar.TabIndex = 1;
+            this.btContinuar.Text = "Continuar";
+            this.btContinuar.UseVisualStyleBackColor = true;
+            // 
+            // btAyuda
+            // 
+            this.btAyuda.Location = new System.Drawing.Point(23, 184);
+            this.btAyuda.Name = "btAyuda";
+            this.btAyuda.Size = new System.Drawing.Size(174, 40);
+            this.btAyuda.TabIndex = 2;
+            this.btAyuda.Text = "Ayuda";
+            this.btAyuda.UseVisualStyleBackColor = true;
+            // 
+            // btRecords
+            // 
+            this.btRecords.Location = new System.Drawing.Point(23, 246);
+            this.btRecords.Name = "btRecords";
+            this.btRecords.Size = new System.Drawing.Size(174, 40);
+            this.btRecords.TabIndex = 3;
+            this.btRecords.Text = "Records";
+            this.btRecords.UseVisualStyleBackColor = true;
+            // 
+            // btSalir
+            // 
+            this.btSalir.Location = new System.Drawing.Point(216, 334);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(82, 28);
+            this.btSalir.TabIndex = 4;
+            this.btSalir.Text = "Salir";
+            this.btSalir.UseVisualStyleBackColor = true;
+            this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(525, 385);
+            this.Controls.Add(this.btSalir);
+            this.Controls.Add(this.btRecords);
+            this.Controls.Add(this.btAyuda);
+            this.Controls.Add(this.btContinuar);
+            this.Controls.Add(this.btIniciar);
+            this.Name = "Form1";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btIniciar;
+        private System.Windows.Forms.Button btContinuar;
+        private System.Windows.Forms.Button btAyuda;
+        private System.Windows.Forms.Button btRecords;
+        private System.Windows.Forms.Button btSalir;
     }
 }
 
