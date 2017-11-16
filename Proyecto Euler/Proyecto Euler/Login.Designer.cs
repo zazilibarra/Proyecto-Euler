ae6e5e52cb5508b40595f88bc4c9b359f260e6bb
k2rosvall <k2.rosas.v@gmail.com>
2017-11-16 15:12:26 -0600

    login
    ....

--- Proyecto Euler/Proyecto Euler/Login.Designer.cs
+++ Proyecto Euler/Proyecto Euler/Login.Designer.cs
@@ -28,11 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btRegresar = new System.Windows.Forms.Button();
+            this.btIniciarSesion = new System.Windows.Forms.Button();
+            this.lbUser = new System.Windows.Forms.Label();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btRegresar
+            // 
+            this.btRegresar.Location = new System.Drawing.Point(12, 344);
+            this.btRegresar.Name = "btRegresar";
+            this.btRegresar.Size = new System.Drawing.Size(106, 29);
+            this.btRegresar.TabIndex = 0;
+            this.btRegresar.Text = "Regresar";
+            this.btRegresar.UseVisualStyleBackColor = true;
+            // 
+            // btIniciarSesion
+            // 
+            this.btIniciarSesion.Location = new System.Drawing.Point(194, 279);
+            this.btIniciarSesion.Name = "btIniciarSesion";
+            this.btIniciarSesion.Size = new System.Drawing.Size(121, 29);
+            this.btIniciarSesion.TabIndex = 1;
+            this.btIniciarSesion.Text = "Iniciar Sesion";
+            this.btIniciarSesion.UseVisualStyleBackColor = true;
+            this.btIniciarSesion.Click += new System.EventHandler(this.btIniciarSesion_Click);
+            // 
+            // lbUser
+            // 
+            this.lbUser.AutoSize = true;
+            this.lbUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUser.Location = new System.Drawing.Point(77, 132);
+            this.lbUser.Name = "lbUser";
+            this.lbUser.Size = new System.Drawing.Size(55, 16);
+            this.lbUser.TabIndex = 2;
+            this.lbUser.Text = "Usuario";
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPassword.Location = new System.Drawing.Point(77, 188);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(77, 16);
+            this.lbPassword.TabIndex = 3;
+            this.lbPassword.Text = "Contraseña";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(181, 132);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(180, 20);
+            this.tbUser.TabIndex = 4;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(181, 188);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(180, 20);
+            this.tbPassword.TabIndex = 5;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(525, 385);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbUser);
+            this.Controls.Add(this.btIniciarSesion);
+            this.Controls.Add(this.btRegresar);
+            this.Name = "Login";
             this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btRegresar;
+        private System.Windows.Forms.Button btIniciarSesion;
+        private System.Windows.Forms.Label lbUser;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbPassword;
     }
 }
--- Proyecto Euler/Proyecto Euler/Login.cs
+++ Proyecto Euler/Proyecto Euler/Login.cs
@@ -12,9 +12,22 @@ namespace Proyecto_Euler
 {
     public partial class Login : Form
     {
+        string usuario;
+        string password;
+
         public Login()
         {
             InitializeComponent();
+            usuario = "";
+            password = "";
+        }
+
+        private void btIniciarSesion_Click(object sender, EventArgs e)
+        {
+            usuario = tbUser.Text;
+            password = tbUser.Text;
+            
+
         }
     }
 }
